4f435699097084f74eeb4563a07e597628529426
Aziz Mamasaliev <aziko.mz@gmail.com>
2021-06-05 18:47:58 +0300

    Partic with property

--- oop_lesson_11.py
+++ oop_lesson_11.py
@@ -1,12 +1,30 @@
 # Практика с property
+from string import digits
+
+
 class User:
     def __init__(self, login, password):
         self.__login = login
-        self.__password = password
+        self.password = password            # сразу вызывается setter
+        self.__secret = '123456'
+
+#    @property
+#    def secret(self):
+#        s = input('Type your password: ')
+#        if s == self.password:
+#            return self.__secret
+#        raise ValueError('Permission denied!')
 
     @property
     def password(self):
         return self.__password
+
+    @staticmethod
+    def is_include_number(password):
+        for i in digits:
+            if i in password:
+                return True
+        return False
 
     @password.setter
     def password(self, new_val):
@@ -16,5 +34,11 @@ class User:
             raise ValueError('Length must be more than 4')
         if len(new_val) >= 13:
             raise ValueError('Length must be less than 12 or equal')
+        if not User.is_include_number(new_val):
+            raise ValueError('Value must consists at least one digit')
         self.__password = new_val
 
+
+a = User('aaa', 'aaa12')
+print(a.password)
+# a.secret
